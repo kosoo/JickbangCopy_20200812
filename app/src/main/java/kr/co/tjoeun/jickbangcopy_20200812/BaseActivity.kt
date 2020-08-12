079c4c59dd5c1b92a6cf015a1dda1a451dce95e5
이용찬 <lee9890@naver.com>
2020-08-12 19:56:50 +0900

    BaseActivity로 코드 정리 기초 작업 수행

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/BaseActivity.kt
@@ -0,0 +1,18 @@
+package kr.co.tjoeun.jickbangcopy_20200812
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+
+//    어느 화면인지 알려줄때 쓰는 용도의 변수 mContext
+    val mContext = this
+
+//    이벤트 처리 코드를 모아두게될 함수
+    abstract fun setupEvents()
+
+//    화면에 데이터를 뿌리는데 관련된 코드를 모아두게될 함수
+    abstract fun setValues()
+
+
+}
--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/MainActivity.kt
@@ -3,9 +3,22 @@ package kr.co.tjoeun.jickbangcopy_20200812
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+//        메인 화면의 이벤트 관련 코드 모아둘 장소
+    }
+
+    override fun setValues() {
+
+//        메인 화면의 값 출력 관련 코드 모아둘 장소
     }
 }
