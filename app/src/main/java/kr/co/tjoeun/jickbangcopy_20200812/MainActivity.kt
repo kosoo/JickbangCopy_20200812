f51a61bee85615ca5ea82e95b1084a62332e736e
이용찬 <lee9890@naver.com>
2020-08-12 21:52:29 +0900

    방목록 ArrayList + 실제방 리스트

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/MainActivity.kt
@@ -2,8 +2,11 @@ package kr.co.tjoeun.jickbangcopy_20200812
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.jickbangcopy_20200812.datas.Room
 
 class MainActivity : BaseActivity() {
+
+    val mRoomList = ArrayList<Room>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +23,17 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
 //        메인 화면의 값 출력 관련 코드 모아둘 장소
+
+//        방 목록을 강제로 추가하는 코드
+        mRoomList.add(Room(8500, "서울시 은평구", 5, "은평구의 5층 방입니다."))
+        mRoomList.add(Room(78000, "서울시 은평구", 3, "은평구의 3층 방입니다."))
+        mRoomList.add(Room(2400, "서울시 은평구", 0, "은평구의 반지하 방입니다."))
+        mRoomList.add(Room(23500, "서울시 서대문구", -1, "서대문구의 지하1층 방입니다."))
+        mRoomList.add(Room(17500, "서울시 서대문구", 4, "서대문구의 4층 방입니다."))
+        mRoomList.add(Room(55000, "서울시 서대문구", 7, "서대문구의 7층 방입니다."))
+        mRoomList.add(Room(9600, "서울시 동대문구", 0, "동대문구의 반지하 방입니다."))
+        mRoomList.add(Room(38000, "서울시 동대문구", 11, "동대문구의 11층 방입니다."))
+        mRoomList.add(Room(57000, "서울시 동대문구", 12, "동대문구의 12층 방입니다."))
+        mRoomList.add(Room(85000, "서울시 동대문구", 15, "동대문구의 15층 방입니다."))
     }
 }
