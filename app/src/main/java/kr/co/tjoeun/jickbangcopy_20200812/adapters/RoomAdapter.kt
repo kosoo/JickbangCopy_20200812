3c3fddb9378f2afda73c86f3e8b4a19d68b67a44
이용찬 <lee9890@naver.com>
2020-08-12 21:17:36 +0900

    어댑터뷰 추가 -> 뼈대 작업만 우선 진행

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/adapters/RoomAdapter.kt
@@ -0,0 +1,29 @@
+package kr.co.tjoeun.jickbangcopy_20200812.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.jickbangcopy_20200812.R
+import kr.co.tjoeun.jickbangcopy_20200812.datas.Room
+
+class roomAdapter(
+    val mContext : Context ,
+    val resId : Int,
+    val mList : List<Room>
+) : ArrayAdapter<Room>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+}
