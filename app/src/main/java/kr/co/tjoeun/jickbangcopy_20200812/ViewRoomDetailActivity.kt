f3a28b5a4b64e6279d57a88635a19368034ad3d5
이용찬 <lee9890@naver.com>
2020-08-13 21:51:51 +0900

    상세보기 화면 주석 완료

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/ViewRoomDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/ViewRoomDetailActivity.kt
@@ -18,8 +18,12 @@ class ViewRoomDetailActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        메인화면에서 넘겨준 room 가져오기
+//        넘겨줄때 Serializable 형태로 넘겨줌 => Room으로 변환해서 저장해야 사용 가능
+//        as Room  추가
         val room = intent.getSerializableExtra("roomInfo") as Room
 
+//        실제 데이터 반영
         priceTxt.text = room.getFormattedPrice()
         descTxt.text = room.description
         addressTxt.text = room.address
