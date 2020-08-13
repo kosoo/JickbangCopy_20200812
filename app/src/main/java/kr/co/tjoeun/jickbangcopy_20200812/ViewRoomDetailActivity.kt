2ca826f3b55778305a11abdcef19b4d38454847b
이용찬 <lee9890@naver.com>
2020-08-13 19:56:51 +0900

    방 상세 정보 조회화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/ViewRoomDetailActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.jickbangcopy_20200812
+
+import android.os.Bundle
+
+class ViewRoomDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}
